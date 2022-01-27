59ec1173435e46c5f2e30597989d37e2c1bdeb50
Mike van der Velde <MikeJordy1999@gmail.com>
2022-01-27 21:49:26 +0100

    Exceptions toegevoegd voor Ledger interacties. Methodes om vorige/volgende transacties en blokken te vinden. En een blockchain verificatie functie.

---
+++ Core/Ledger/Exceptions/DuplicateException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Ledger.Exceptions {
+	public class DuplicateException : Exception {
+		public DuplicateException() { }
+		public DuplicateException(string message) : base(message) { }
+	}
+}
---
+++ Core/Ledger/Exceptions/InvalidBlockException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Ledger.Exceptions {
+	public class InvalidBlockException : Exception {
+		public InvalidBlockException() { }
+		public InvalidBlockException(string message) : base(message) { }
+	}
+}
